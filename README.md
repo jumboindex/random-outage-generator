e74866a53112ab1349b2722a3d9d5c428d6f009b
James Ball <82158878+jumboindex@users.noreply.github.com>
2021-04-13 20:13:16 +0100

    second revision

--- README.md
+++ README.md
@@ -1,43 +1,24 @@
-# Random message / outage generator
->     The script will output a random data centre out via the console each time it's run.
-
 ## Table of contents
-+
-+
-+
-+
-
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [To-do list](#to-do-list)
+* [Status](#status)
+ 
 ## General info
-
-This is a random message generator which I have created to learn Git and Github as part of my code accedemy course. The inspiration for each of the random messages / outages comes from my experience working in data centres (yes this has all happened at undisclosed companies). It could easily be modified to generate random service outages to put on a status page or distribute via email to a customer list.
-
-
-## Screenshots
-![Example screenshot](./img/screenshot.png)
-
+ 
+This is a random message generator which I've created to learn Git and Github as part of my code academy course. The inspiration for each of the random messages / outages comes from my experience working in data centres. It could easily be modified to put a message on a status page or distribute via email to a customer list.
+ 
 ## Technologies
 * Java Script 
-
+ 
 ## Setup
-Nothing complicated:
-
 ```
 node script.js 
 ```
-## Code Examples
-Show examples of usage:
-`put-your-code-here`
-
-## Features
-List of features ready and TODOs for future development
-* Awesome feature 1
-* Awesome feature 2
-* Awesome feature 3
-
-To-do list:
-* Wow improvement to be done 1
-* Wow improvement to be done 2
-
+## To-do list:
+* add email functionality. 
+* output to front end / status page.
+ 
 ## Status
 Project is: _in progress_
-
--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-
-
-
+// Simple script to generate random messages / outages in a data centre.
+ 
+// Store possible failure scenarios in an object. 
 const outageMessages = {
  
-   service:['UPS system', 'generators', 'cooling system', 'core network infrastructure', 'fire suppression system'],
-   status:['offline', 'partially degraded', 'blown up'],
-   impact:['reduced redundancy','reduced resilency', 'impacted', 'unavailable']
+   service:['UPS system', 'generators', 'cooling systems', 'core network infrastructure', 'fire suppression system'],
+   status:['reduced redundancy','reduced resiliency', 'impacted', 'unavailable'],
+   rootCause:['a cat in the data centre', 'typographical error', 'cascading failure event', 'Human error', 'The generators ran out of fuel', 'a UPS module exploded'],
+   advice:['initiate your disaster recovery plans', 'sit tight and pray', 'switch your laptop off for the day'] 
 }
-
-
+ 
+// Random number generator function.
 const generateRandomNumber = (number) => Math.floor(Math.random() * number);
  
+// Message generator function => loop through object properties, random number to pull eventID and push to messageArray. Return template literal string. 
 const generateRandomOutage = () => {
    let messageArray = [];
    for (let property in outageMessages){
       let eventID = generateRandomNumber(outageMessages[property].length)
       messageArray.push(outageMessages[property][eventID])
    }
-
-
-   return `Dear Customer, we are curruently experincing issues with our: ${messageArray[0]}. The current status of our ${messageArray[0]} is: ${messageArray[1]}. Please expect the following impact to your services: ${messageArray[2]}. We advice that you initiate your disaster recovery plans.`
-    
+let outageNotifcation = `Dear Customer, we are currently experiencing issues with our: ${messageArray[0]}. The current status of our ${messageArray[0]} is: ${messageArray[1]}. The root cause of this failure is: ${messageArray[2]}. We advice that you ${messageArray[3]}.`
+ 
+   return outageNotifcation
  };
-
-
-
  
 console.log(generateRandomOutage());
